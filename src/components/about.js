cbeb2ee400f2684b6636562099640f8774d9506e
Telson <ggbigbang@gmail.com>
2021-10-17 22:05:24 +0800

    service

--- src/components/about.js
+++ src/components/about.js
@@ -14,9 +14,10 @@ const Header = ({ data }) => (
         </div>
         <div className="left col-md-7 col-lg-8">
           <div className="about-details">
-            <span className="name">My Name is {data.name}.</span>
+            {/* <span className="name">My Name is {data.name}.</span> */}
             <h2 className="sub-position">
-              I'm a {data.designation}.
+              {/* I'm a {data.designation}. */}
+              I'm Telson, a {data.designation}.
             </h2>
             <div
               dangerouslySetInnerHTML={{
