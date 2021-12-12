32a601c91cb696bc63909ef6df4b522fa8748cb8
Telson <ggbigbang@gmail.com>
2021-12-12 23:13:10 +0800

    edited testimonials

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,7 @@ export default class footer extends Component {
       <div className="site-footer" id="footer">
         <div className="container">
           <span>{this.props.siteName}</span>
+          <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
       </div>
     );
